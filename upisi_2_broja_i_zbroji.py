407703f44b3f6dd5a4c18116acba9c357e4dd64b
fumarela <fumarela@gmail.com>
2019-01-22 17:27:44 +0100

    zbroj 2 broja

---
+++ upisi_2_broja_i_zbroji.py
@@ -0,0 +1,17 @@
+
+print("Upisi prvi broj:")
+
+prvi = int(input())
+
+print("Upisi drugi broj:")
+
+drugi = int(input())
+
+print ("Upisao si brojeve: ", str(prvi) , "i " , str(drugi))
+
+zbroj  = prvi + drugi
+
+print ("Njihov zbroj recimo je: ", zbroj)
+print ("Njihov zbroj drugacije je: ", int(prvi-drugi))
+print ("Njihov zbroj drugacije je: ", (prvi-drugi))
+
