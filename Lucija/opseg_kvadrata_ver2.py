f1b3fae464cc4266d3d63548853fb70b1db18d32
fumarela <fumarela@gmail.com>
2019-01-23 08:22:05 +0100

    primjer opsega kvadrata

---
+++ Lucija/opseg_kvadrata_ver2.py
@@ -0,0 +1,12 @@
+
+
+
+print("Upiši mi molim kolika je duljina jedne stranice kvadrata! *************** ")
+
+#varijabla = to što ja upišem----
+a = int(input())
+
+# O kvadrata je zapravo formula O = 4 * a
+
+o = 4 * a
+print("Opseg kvadrata je : ", o)
