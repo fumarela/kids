c7955e72def5f192ed6085bff3e13c4e963c8c5d
fumarela <fumarela@gmail.com>
2019-02-04 09:53:49 +0100

    dodan način logičkog ispitivanja da li je broj paran ili neparan u file parni_brojevi.py

--- test/parni_brojevi.py
+++ test/parni_brojevi.py
@@ -18,7 +18,7 @@ for x in range(10, 20, 2):
 # Poslije recimo neparne.
 
 # pitaj korisnika
-print("Upisi neki broj")
+print("Upisi neki pozitivan broj veći od 0...")
 
 x = int(input())
 
@@ -28,13 +28,37 @@ x = int(input())
 
 # ispiši SVE pozitivne brojeve od 0 do TOG broja, SAMO PARNE
 # primjeti brojeve u range i razmisli zašto su takvi
+print ("\nSvi PARNI brojevi do TVOG zamisljenog broja su :\n" )
 for neki_broj in range(2, x, 2):
-    print ("neki_broj je :" , neki_broj)
+    print ("parni broj je :" , neki_broj)
 
 
+print ("\nSvi NEPARNI brojevi do TVOG zamisljenog broja su :\n" )
 # ispiši SVE od 0 do TOG broja, ali ovaj puta SAMO NEPARNE
 for neki_broj in range(1, x, 2):
-    print ("neki_broj je :" , neki_broj)
+    print ("neparni broj je :" , neki_broj)
+
+# drugi način ispisa parnih i neparnih brojeve uključuje
+# logičko ispitivanje postoji li OSTATAK kod dijeljenja sa brojem 2.
+# ukoliko postoji ostatak kod dijeljenja - broj je neparan, zar ne? ... 7 % 2 = 1 --> dakle ima ostatak
+# ukoliko ne postoji ostatak kod takvog dijeljenja - broj je sigurno paran. primjer: 8 % 2 = 0  --> nema ostatka
+# kako to logičko ispitivanje napisati u obliku python koda?
+# 
+#  if (nekibroj % 2 == 1)
+#       broja nam je paran
+# else
+#       broj nam je neparan. 
+# dakle kod bi mogao biti....
+
+print ("\nDrugi način\n\nSvi parni i neparni brojevi do TVOG zamisljenog broja su :\n" )
+for neki_broj in range(x):
+    if neki_broj == 0:           # nulu ćemo preskočiti u petlji, ona nije ni parna ni neparna, zar ne?
+        continue
+    elif (neki_broj %2 == 1):    # razumijemo zašto?
+        print ("broj je neparan: " , neki_broj)
+    else:                        # ako nije ništa odozora još, onda samo može biti još neparan, zar ne?
+        print ("broj je paran: " , neki_broj)
+
 
 
 # nastavi kasnije sa jos kojim primjerom sam
